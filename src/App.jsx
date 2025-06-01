6d27c171d9ae19ef088d67f4384aeba4c3c2c87d
abuzarraziqgithub <abuzarraziqo@gmail.com>
2025-06-01 10:20:11 +0500

    opti,ized and added colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,83 @@ import { useState } from "react";
 function App() {
   const [color, setColor] = useState("olive");
 
+  const handleColorChange = (color) => {
+    setColor(color);
+  };
+
   return (
     <>
       <div
-        className="w-full h-screen flex justify-center items-end"
+        className="w-full h-screen flex justify-center items-end transition-all duration-500"
         style={{ backgroundColor: color }}
       >
-        <div className="bg-neutral-50 sm:w-2/4 flex justify-center sm:h-24 mb-10 rounded-xl ">
-          <div className="flex sm:justify-around sm:w-full p-5 flex-wrap">
+        <div className="bg-neutral-50 w-auto  flex justify-center h-auto mb-10 rounded-xl  ">
+          <div className="flex justify-around w-full p-5 gap-3 flex-wrap   ">
             <button
               className="bg-red-700 sm:p-5 sm:h-16 rounded-xl w-20 outline-none"
-              onClick={() => setColor("red")}
+              onClick={() => handleColorChange("red")}
             >
               Red
             </button>
             <button
               className="bg-green-700 p-5 h-16 rounded-xl w-20 outline-none"
-              onClick={() => setColor("green")}
+              onClick={() => handleColorChange("green")}
             >
               Green
             </button>
             <button
               className="bg-blue-700 p-5 h-16 rounded-xl w-20 outline-none"
-              onClick={() => setColor("blue")}
+              onClick={() => handleColorChange("blue")}
             >
               Blue
             </button>
             <button
               className="bg-yellow-500 p-5 h-16 rounded-xl w-20 outline-none"
-              onClick={() => setColor("yellow")}
+              onClick={() => handleColorChange("yellow")}
             >
               Yellow
+            </button>
+            <button
+              className="bg-purple-500 p-5 h-16 rounded-xl w-20 outline-none"
+              onClick={() => handleColorChange("purple")}
+            >
+              Purple
+            </button>
+            <button
+              className="bg-orange-500 p-5 h-16 rounded-xl w-20 outline-none"
+              onClick={() => handleColorChange("orange")}
+            >
+              Orange
+            </button>
+            <button
+              className="bg-black p-5 h-16 rounded-xl w-20 text-sky-50 outline-none"
+              onClick={() => handleColorChange("black")}
+            >
+              Black
+            </button>
+            <button
+              className="bg-white p-5 h-16 rounded-xl w-20 outline-none border border-zinc-700"
+              onClick={() => handleColorChange("white")}
+            >
+              White
+            </button>
+            <button
+              className="bg-pink-500 p-5 h-16 rounded-xl w-20 outline-none"
+              onClick={() => handleColorChange("pink")}
+            >
+              Pink
+            </button>
+            <button
+              className="bg-gray-500 p-5 h-16 rounded-xl w-20 outline-none text-red-50"
+              onClick={() => handleColorChange("gray")}
+            >
+              Gray
+            </button>
+            <button
+              className="bg-amber-900 p-5 h-16 rounded-xl w-20 outline-none text-red-50"
+              onClick={() => handleColorChange("brown")}
+            >
+              Amber
             </button>
           </div>
         </div>
